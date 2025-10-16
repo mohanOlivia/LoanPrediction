ce92bec2d9ce48136e0e44cbb39a65118ef05549
Olivia <162891121+mohanOlivia@users.noreply.github.com>
2025-10-16 16:28:45 +0100

    Handling missing values in df

--- src/data_processing.py
+++ src/data_processing.py
@@ -6,4 +6,17 @@ def load_data(path):
 
 
 def fill_missing_values(df):
+
+    # Categorical    
+    for col in ["Gender", "Married", "Self_Employed"]:
+        df[col] = df[col].fillna(df[col].mode()[0], inplace=True)
+
+
+    # Numerical 
+    for col in ["LoanAmount", "Loan_Amount_Term"]:
+        df[col] = df[col].fillna(df[col].median(), inplace=True)
+
+    # Credit History - Boolean [Take mode of all booleans]
+    df['Credit_History'].fillna(df['Credit_History'].mode()[0], inplace=True)
+
     return df
